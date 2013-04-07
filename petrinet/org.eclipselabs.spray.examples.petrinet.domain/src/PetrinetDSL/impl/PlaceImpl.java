6202c8754727814b7a762914d307e010594c8951
Florian Fendt <flfendt@htwg-konstanz.de>
2013-04-07 09:46:59 +0200

    add .project

--- petrinet/org.eclipselabs.spray.examples.petrinet.domain/src/PetrinetDSL/impl/PlaceImpl.java
+++ petrinet/org.eclipselabs.spray.examples.petrinet.domain/src/PetrinetDSL/impl/PlaceImpl.java
@@ -18,6 +18,7 @@ import org.eclipse.emf.ecore.InternalEObject;
 
 import org.eclipse.emf.ecore.util.EObjectContainmentEList;
 import org.eclipse.emf.ecore.util.EObjectResolvingEList;
+import org.eclipse.emf.ecore.util.EObjectWithInverseResolvingEList;
 import org.eclipse.emf.ecore.util.InternalEList;
 
 /**
